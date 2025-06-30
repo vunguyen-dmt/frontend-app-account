8459e80610d47939922da1d3590443072b54e2c6
Vu Nguyen <vu.nguyen@dmttek.com>
2025-06-30 12:36:24 +0700

    lock some fields and add Vietnamese

--- src/account-settings/data/selectors.js
+++ src/account-settings/data/selectors.js
@@ -143,7 +143,7 @@ export const staticFieldsSelector = createSelector(
   mostRecentVerifiedNameSelector,
   (accountSettings, verifiedName) => {
     const staticFields = [];
-    if (accountSettings.profileDataManager) {
+    if (true || accountSettings.profileDataManager) {
       staticFields.push('name', 'email', 'country');
     }
     if (verifiedName && ['submitted'].includes(verifiedName.status)) {
